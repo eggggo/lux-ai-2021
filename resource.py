41ffffa9ab8885bd974010182aeb354fd1f2c843
Ethan Wong <fireshot90@gmail.com>
2021-08-23 17:39:14 -0400

    improved resource collection algorithm

--- resource.py
+++ resource.py
@@ -7,7 +7,7 @@ from lux.game_constants import GAME_CONSTANTS
 from lux import annotate
 
 
-def findOptimalResource(map, researchPoints, unit):
+def findOptimalResource(map, researchPoints, unit, turns_before_night):
     width, height = map.width, map.height
 
     def in_bounds(pos):
@@ -16,9 +16,13 @@ def findOptimalResource(map, researchPoints, unit):
         else:
             return False
 
-    def valueFunction(fuelAmount, distance):
-        return fuelAmount - 5*distance
+    z = 5 # turns before night that I want to make profit by
 
+    # def valueFunction(fuelAmount, turns_to_reach_resource, collection_rate_closest_to_resource, turns_to_closest_to_resource):
+    #     return fuelAmount*(turns_before_night - z - turns_to_reach_resource-turns_to_closest_to_resource) - collection_rate_closest_to_resource*turns_to_reach_resource
+
+    def valueFunction(fuelAmount, turns_to_reach_resource):
+        return fuelAmount*(turns_before_night - z - turns_to_reach_resource) - 40*turns_to_reach_resource
     resourceMap: list[list[int]] = [[0 for c in range(width)] for r in range(height)]
     for y in range(height):
         for x in range(width):
@@ -46,12 +50,32 @@ def findOptimalResource(map, researchPoints, unit):
             if (in_bounds(Position(x, y + 1))):
                 fuelCollectionMap[x][y] += resourceMap[x][y + 1]
 
+    def abs_value(numerical_value):
+        if numerical_value < 0:
+            return numerical_value*-1
+        else:
+            return numerical_value
+
+    # def find_closest_resource(pos):
+    #     max_dist = math.inf
+    #     closest_position = Position(0, 0)
+    #     for y in range(height):
+    #         for x in range(width):
+    #             dist = math.sqrt((pos.x-x)**2 + (pos.y-y)**2)
+    #             if dist < max_dist and fuelCollectionMap[x][y] != 0:
+    #                 closest_position = Position(x, y)
+    #     return closest_position
+
     valueList: list[(Position, int)] = []
     for y in range(height):
         for x in range(width):
             if (fuelCollectionMap[x][y] > 0):
-                distanceTo = unit.pos.distance_to(Position(x, y))
-                value = valueFunction(fuelCollectionMap[x][y], distanceTo)
+                turns_to_destination = 2*(abs_value(unit.pos.x-x) + abs_value(unit.pos.y-y))
+                # closest_resource_position = find_closest_resource(Position(x, y))
+                # collection_rate_closest_resource = fuelCollectionMap[closest_resource_position.x][closest_resource_position.y]
+                # turns_to_closest_resource = 2*(abs_value(closest_resource_position.x-x) + abs_value(closest_resource_position.y-y))
+                # value = valueFunction(fuelCollectionMap[x][y], turns_to_destination, collection_rate_closest_resource, turns_to_closest_resource)
+                value = valueFunction(fuelCollectionMap[x][y], turns_to_destination)
                 valueList.append((Position(x, y), value))
     
     def key(item):
