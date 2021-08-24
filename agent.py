5509163bf0d490408e6e492577c01d7fbb400a8b
andli <andli@umich.edu>
2021-08-24 01:46:42 -0400

    changed wood_reliance to 0, renamed resource.py to fuel.py since its an existing package

--- agent.py
+++ agent.py
@@ -5,7 +5,7 @@ from lux.game_map import Cell, RESOURCE_TYPES, Position
 from lux.constants import Constants
 from lux.game_constants import GAME_CONSTANTS
 from lux import annotate
-from resource import findOptimalResource
+from fuel import findOptimalResource
 
 DIRECTIONS = Constants.DIRECTIONS
 game_state = None
@@ -206,7 +206,7 @@ def agent(observation, configuration):
     if game_state.turn == 0:
         wood_on_map_initial = wood_on_map
     threshold_use_other = .4
-    wood_reliance = 80
+    wood_reliance = 0
     if wood_on_map_initial*threshold_use_other >= wood_on_map:
         wood_reliance = 0
     #list of tiles with adjacent tiles of more than 1 city
